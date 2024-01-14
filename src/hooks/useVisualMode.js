5bddd773ecca5428e2ec5f061dc7cc63689610a1
Hisban Shiraz <hisbanbardai@gmail.com>
2024-01-14 11:54:24 -0700

    Add a transition function and a corresponding test for it

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,8 +3,13 @@ import { useState } from "react";
 export default function useVisualMode(initialMode) {
   const [mode, setMode] = useState(initialMode);
 
+  function transition(newMode) {
+    setMode(newMode);
+  }
+
   return {
-    mode,
     //we can write only mode too instead of mode: mode because name of key and value is same
+    mode,
+    transition,
   };
 }
