98d88ef7da9ad96bf70ca4dfd4fd5166b1dca3b2
Hisban Shiraz <hisbanbardai@gmail.com>
2024-01-15 10:15:53 -0700

    Implement back function and add tests for it

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,15 +1,21 @@
 import { useState } from "react";
 
 export default function useVisualMode(initialMode) {
-  const [mode, setMode] = useState(initialMode);
+  // const [mode, setMode] = useState(initialMode);
+  //we do not need to set state of mode separately because we can derive the mode from history array i.e. the last item in the history array which basically would be at the top of the stack because stack is LIFO
+  const [history, setHistory] = useState([initialMode]);
 
   function transition(newMode) {
-    setMode(newMode);
+    setHistory((prevMode) => [...prevMode, newMode]);
+  }
+
+  function back() {
+    setHistory((prevMode) => prevMode.slice(0, prevMode.length - 1));
   }
 
   return {
-    //we can write only mode too instead of mode: mode because name of key and value is same
-    mode,
+    mode: history[history.length - 1],
     transition,
+    back,
   };
 }
