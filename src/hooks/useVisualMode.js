307b5156f389fcc866117206ad7c91fa00f01504
Hisban Shiraz <hisbanbardai@gmail.com>
2024-01-15 10:48:18 -0700

    Implement limits and add tests for it

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -5,12 +5,18 @@ export default function useVisualMode(initialMode) {
   //we do not need to set state of mode separately because we can derive the mode from history array i.e. the last item in the history array which basically would be at the top of the stack because stack is LIFO
   const [history, setHistory] = useState([initialMode]);
 
-  function transition(newMode) {
-    setHistory((prevMode) => [...prevMode, newMode]);
+  function transition(newMode, replace = false) {
+    replace
+      ? setHistory((prevMode) => [
+          ...prevMode.slice(0, prevMode.length - 1),
+          newMode,
+        ])
+      : setHistory((prevMode) => [...prevMode, newMode]);
   }
 
   function back() {
-    setHistory((prevMode) => prevMode.slice(0, prevMode.length - 1));
+    if (history.length > 1)
+      setHistory((prevMode) => prevMode.slice(0, prevMode.length - 1));
   }
 
   return {
