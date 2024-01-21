341f79a93a4dab75e1bb5347960cfc9bf7c7f3da
Hisban Shiraz <hisbanbardai@gmail.com>
2024-01-21 14:12:24 -0700

    Implement fifth test 'calls onSave function when the name and interviewer is defined'

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -33,7 +33,7 @@ describe("Form", () => {
     /* 1. Create the mock onSave function */
     const onSave = jest.fn();
 
-    /* 2. Render the Form with interviewers and the onSave mock function passed as an onSave prop, the student prop should be blank or undefined */
+    /* 2. Render the Form with interviewers and the onSave mock function passed as an onSave prop, the name prop should be blank or undefined */
     const { getByText } = render(
       <Form interviewers={interviewers} onSave={onSave} />
     );
@@ -49,13 +49,12 @@ describe("Form", () => {
     /* 1. Create the mock onSave function */
     const onSave = jest.fn();
 
-    /* 2. Render the Form with interviewers and the onSave mock function passed as an onSave prop, the interviewer prop should be null */
+    /* 2. Render the Form with interviewers and the onSave mock function passed as an onSave prop, the name prop should be blank or undefined */
     const { getByText } = render(
       <Form
         interviewers={interviewers}
         onSave={onSave}
         name="Lydia Miller-Jones"
-        interviewer={null}
       />
     );
 
@@ -70,9 +69,18 @@ describe("Form", () => {
     /* 1. Create the mock onSave function */
     const onSave = jest.fn();
 
-    /* 2. Render the Form with interviewers, name and the onSave mock function passed as an onSave prop */
+    /* 2. Render the Form with interviewers, student name and the onSave mock function passed as an onSave prop */
+    const { getByText, queryByText } = render(
+      <Form
+        interviewers={interviewers}
+        onSave={onSave}
+        name="Lydia Miller-Jones"
+        interviewer={interviewers[0].id}
+      />
+    );
 
     /* 3. Click the save button */
+    fireEvent.click(getByText("Save"));
 
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
     expect(queryByText(/please select an interviewer/i)).toBeNull();
