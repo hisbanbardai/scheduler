4e6c8a905a546a72d8d34a109571acf20d857563
Hisban Shiraz <hisbanbardai@gmail.com>
2024-01-21 12:47:51 -0700

    Add a test file for Form component with starter code

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -17,6 +17,8 @@ import Appointment from "components/Appointment";
 /*
   A test that renders a React Component
 */
-it("renders without crashing", () => {
-  render(<Appointment />);
+describe("Appointment", () => {
+  it("renders without crashing", () => {
+    render(<Appointment />);
+  });
 });
