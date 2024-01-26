edf8ec0fe38ec216236a35c257675ca83c55e475
Hisban Shiraz <hisbanbardai@gmail.com>
2024-01-26 16:53:49 -0700

    Add test step: Click the Delete button on the booked appointment

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -11,6 +11,7 @@ import {
   getByAltText,
   getByPlaceholderText,
   queryByText,
+  queryByAltText,
 } from "@testing-library/react";
 
 import Application from "components/Application";
@@ -70,11 +71,18 @@ describe("Form", () => {
     // 1. Render the Application.
     const { container, debug } = render(<Application />);
     // 2. Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, "Archie Cohen"));
     // 3. Click the "Delete" button on the booked appointment.
+    const appointment = getAllByTestId(container, "appointment").find(
+      (appointment) => queryByText(appointment, "Archie Cohen")
+    );
+
+    fireEvent.click(queryByAltText(appointment, "Delete"));
     // 4. Check that the confirmation message is shown.
     // 5. Click the "Confirm" button on the confirmation.
     // 6. Check that the element with the text "Deleting" is displayed.
     // 7. Wait until the element with the "Add" button is displayed.
     // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
+    debug();
   });
 });
