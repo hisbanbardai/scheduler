3ecf600e034a37aae130fbe89b0a2b31eed0d239
Hisban Shiraz <hisbanbardai@gmail.com>
2024-01-26 16:58:20 -0700

    Add test step: Check that the element with the text Deleting is displayed

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -90,6 +90,8 @@ describe("Form", () => {
     fireEvent.click(getByText(appointment, "Confirm"));
 
     // 6. Check that the element with the text "Deleting" is displayed.
+    expect(getByText(appointment, "Deleting")).toBeInTheDocument();
+
     // 7. Wait until the element with the "Add" button is displayed.
     // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
     debug();
