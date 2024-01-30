e7fb924c7d2ff7cb963c089e1dafda13ebc6dbea
Hisban Shiraz <hisbanbardai@gmail.com>
2024-01-30 12:45:20 -0700

    Add a test step: 'Enter student name'

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -3,5 +3,6 @@ describe("Appointments", () => {
     cy.visit("/");
     cy.contains("Monday");
     cy.get("[alt=Add]").first().click();
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
   });
 });
