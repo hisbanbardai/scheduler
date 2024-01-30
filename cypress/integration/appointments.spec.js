ed9b186f86861a12a1d3c2c6390c58b4128a2ac3
Hisban Shiraz <hisbanbardai@gmail.com>
2024-01-30 12:54:48 -0700

    Add beforeEach() method to Refactor the test to separate the common test commands

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,8 +1,11 @@
 describe("Appointments", () => {
-  it("should book an interview", () => {
+  beforeEach(() => {
     cy.request("GET", "/api/debug/reset");
     cy.visit("/");
     cy.contains("Monday");
+  });
+
+  it("should book an interview", () => {
     cy.get("[alt=Add]").first().click();
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
     cy.get("[alt='Sylvia Palmer']").click();
